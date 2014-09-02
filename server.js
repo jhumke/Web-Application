b7aacd210a33920b47a0abd3463bfb69b53a8812
jugash <jugash.ch@gmail.com>
2014-09-02 23:36:51 +0100

    #1 Adding server bootstrap

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var product = require('./routes/products');
+ 
+var app = express();
+
+app.use(express.compress());
+app.use(express.static(__dirname + '/public'), {maxAge: 20});
+app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+app.use(express.bodyParser());
+ 
+app.get('/products', product.findAll);
+app.get('/products/:id', product.findById);
+app.get('/products/c/:category', product.findByCategory);
+app.get('/products/c/any/:category', product.findByAnyCategory);
+app.get('/products/t/all/:tags', product.findByAllTags);
+app.get('/products/t/any/:tags', product.findByAnyTag);
+app.post('/products', product.addProduct);
+app.put('/products/:id', product.updateProduct);
+app.delete('/products/:id', product.deleteProduct);
+ 
+app.listen(3000);
+console.log('Listening on port 3000...');

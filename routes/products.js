32af789a5c64c8c17117689df1674511a4c1b61f
jugash <jugash.ch@gmail.com>
2014-09-02 23:43:43 +0100

    #1 Adding package.json and products domain object

---
+++ routes/products.js
@@ -0,0 +1,144 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('productdb', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'productdb' database");
+        db.collection('products', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'products' collection doesn't exist. Creating it with sample data...");
+                populateDB();
+            }
+        });
+    }
+});
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving product: ' + id);
+    db.collection('products', function(err, collection) {
+        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+            res.send(item);
+        });
+    });
+};
+
+exports.findByCategory = function(req, res) {
+    var category = req.params.category;
+    console.log('Retrieving products for category: ' + category);
+    db.collection('products', function(err, collection) {
+        collection.find({'category': category}).toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.findByAnyCategory = function(req, res) {
+    var categories = req.params.category.split(',');
+    db.collection('products', function(err, collection) {
+        collection.find({'category': { '$in' : categories } }).toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.findByAllTags = function(req, res) {
+    var tags = req.params.tags.split(',');
+    db.collection('products', function(err, collection) {
+        collection.find({'tags' : { '$all' : tags }}).toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.findByAnyTag = function(req, res) {
+    var tags = req.params.tags.split(',');
+    db.collection('products', function(err, collection) {
+        collection.find({'tags' : { '$in' : tags }}).toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.findAll = function(req, res) {
+    db.collection('products', function(err, collection) {
+        collection.find().toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
+
+exports.addProduct = function(req, res) {
+    var product = req.body;
+    console.log('Adding product: ' + JSON.stringify(product));
+    db.collection('products', function(err, collection) {
+        collection.insert(product, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
+}
+
+exports.updateProduct = function(req, res) {
+    var id = req.params.id;
+    var product = req.body;
+    console.log('Updating product: ' + id);
+    console.log(JSON.stringify(product));
+    db.collection('products', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, product, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating product: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(product);
+            }
+        });
+    });
+}
+
+exports.deleteProduct = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting product: ' + id);
+    db.collection('products', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
+}
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+// Populate database with sample data -- Only used once: the first time the application is started.
+// You'd typically not find this code in a real-life app, since the database would already exist.
+var populateDB = function() {
+
+    var products = [
+    {
+        "name" : "Indian Bangle",
+        "price" : "10.50",
+        "category" : "Bangle",
+        "tags" : [ "green", "gold"],
+	    "description" : "This is a red bangle" 
+    }
+    ];
+
+    db.collection('products', function(err, collection) {
+        collection.insert(products, {safe:true}, function(err, result) {});
+    });
+
+};
